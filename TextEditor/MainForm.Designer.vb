e28150327916f28da191492bb63ac494f9c89512
ysiDC <51706931+ysiDC@users.noreply.github.com>
2019-07-25 16:26:19 -0400

    Uploaded NETD lab 5

---
+++ TextEditor/MainForm.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tbMainBox = New System.Windows.Forms.TextBox()
+        Me.mnuMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuPaste = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextEditorTips = New System.Windows.Forms.ToolTip(Me.components)
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.mnuMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tbMainBox
+        '
+        Me.tbMainBox.Location = New System.Drawing.Point(13, 39)
+        Me.tbMainBox.Multiline = True
+        Me.tbMainBox.Name = "tbMainBox"
+        Me.tbMainBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbMainBox.Size = New System.Drawing.Size(646, 532)
+        Me.tbMainBox.TabIndex = 1
+        Me.TextEditorTips.SetToolTip(Me.tbMainBox, "Type or edit text here (contents of an opened file will be displayed here)")
+        '
+        'mnuMenuStrip
+        '
+        Me.mnuMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuEdit, Me.mnuHelp})
+        Me.mnuMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.mnuMenuStrip.Name = "mnuMenuStrip"
+        Me.mnuMenuStrip.Size = New System.Drawing.Size(671, 24)
+        Me.mnuMenuStrip.TabIndex = 0
+        Me.mnuMenuStrip.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuNew, Me.mnuOpen, Me.mnuSave, Me.mnuSaveAs, Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "&File"
+        Me.mnuFile.ToolTipText = "Opens a drop-down menu for creating a new file, opening an existing file, saving," &
+    " and exiting the program"
+        '
+        'mnuNew
+        '
+        Me.mnuNew.Name = "mnuNew"
+        Me.mnuNew.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.mnuNew.Size = New System.Drawing.Size(152, 22)
+        Me.mnuNew.Text = "&New"
+        '
+        'mnuOpen
+        '
+        Me.mnuOpen.Name = "mnuOpen"
+        Me.mnuOpen.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.mnuOpen.Size = New System.Drawing.Size(152, 22)
+        Me.mnuOpen.Text = "&Open"
+        '
+        'mnuSave
+        '
+        Me.mnuSave.Name = "mnuSave"
+        Me.mnuSave.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.mnuSave.Size = New System.Drawing.Size(152, 22)
+        Me.mnuSave.Text = "&Save"
+        '
+        'mnuSaveAs
+        '
+        Me.mnuSaveAs.Name = "mnuSaveAs"
+        Me.mnuSaveAs.Size = New System.Drawing.Size(152, 22)
+        Me.mnuSaveAs.Text = "Save &As"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(152, 22)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'mnuEdit
+        '
+        Me.mnuEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuCopy, Me.mnuCut, Me.mnuPaste})
+        Me.mnuEdit.Name = "mnuEdit"
+        Me.mnuEdit.Size = New System.Drawing.Size(39, 20)
+        Me.mnuEdit.Text = "&Edit"
+        Me.mnuEdit.ToolTipText = "Opens a drop-down menu for copying, cutting, and pasting text"
+        '
+        'mnuCopy
+        '
+        Me.mnuCopy.Name = "mnuCopy"
+        Me.mnuCopy.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.mnuCopy.Size = New System.Drawing.Size(152, 22)
+        Me.mnuCopy.Text = "&Copy"
+        '
+        'mnuCut
+        '
+        Me.mnuCut.Name = "mnuCut"
+        Me.mnuCut.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.mnuCut.Size = New System.Drawing.Size(152, 22)
+        Me.mnuCut.Text = "Cu&t"
+        '
+        'mnuPaste
+        '
+        Me.mnuPaste.Name = "mnuPaste"
+        Me.mnuPaste.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.mnuPaste.Size = New System.Drawing.Size(152, 22)
+        Me.mnuPaste.Text = "&Paste"
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuAbout})
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(44, 20)
+        Me.mnuHelp.Text = "&Help"
+        Me.mnuHelp.ToolTipText = "Opens a drop-down menu of help options"
+        '
+        'mnuAbout
+        '
+        Me.mnuAbout.Name = "mnuAbout"
+        Me.mnuAbout.Size = New System.Drawing.Size(152, 22)
+        Me.mnuAbout.Text = "About"
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(671, 583)
+        Me.Controls.Add(Me.tbMainBox)
+        Me.Controls.Add(Me.mnuMenuStrip)
+        Me.MainMenuStrip = Me.mnuMenuStrip
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Text Editor"
+        Me.mnuMenuStrip.ResumeLayout(False)
+        Me.mnuMenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbMainBox As TextBox
+    Friend WithEvents mnuMenuStrip As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuNew As ToolStripMenuItem
+    Friend WithEvents mnuOpen As ToolStripMenuItem
+    Friend WithEvents mnuSave As ToolStripMenuItem
+    Friend WithEvents mnuSaveAs As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents mnuEdit As ToolStripMenuItem
+    Friend WithEvents mnuCopy As ToolStripMenuItem
+    Friend WithEvents mnuCut As ToolStripMenuItem
+    Friend WithEvents mnuPaste As ToolStripMenuItem
+    Friend WithEvents mnuHelp As ToolStripMenuItem
+    Friend WithEvents mnuAbout As ToolStripMenuItem
+    Friend WithEvents TextEditorTips As ToolTip
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+End Class
---
+++ TextEditor/MainForm.vb
@@ -0,0 +1,174 @@
+﻿'' Program name:        Text Editor
+'' Author:              Simon Yarrow (100738683)
+'' Course:              NETD 2202
+'' Date:                July 25, 2019
+'' Program purpose:     This Windows form is a simple text editor program. A user can type text into a blank text box or load an existing
+''                      text file and edit it (with Open), then save their work using Save or Save As (the latter will prompt for a file
+''                      name to create or overwrite, while the former will overwrite the existing file if there is one or prompt for a file
+''                      name otherwise). Users can Cut, Copy, and Paste text as normal for a Windows program. All of these options (as well
+''                      as a simple pop-up window with information about the program) can be accessed using the mouse from drop-down menus
+''                      or using keyboard shortcuts with the Alt key, and some of them (as noted in the menus) can also be accessed more
+''                      immediately with keyboard shurtcuts using the Ctrl key. A pop-up window will alert the user if something they typed
+''                      is about to be discarded by an action they are taking.
+
+Option Strict On
+Imports System.IO       '' For reading and writing files
+Public Class MainForm
+
+    ''CONSTANTS AND VARIABLES (class-level)
+    Const FormNameDefault As String = "Text Editor"     '' The default text at the top of the window
+    Dim latestFileName As String = ""                   '' The file name of the current file (non-empty only if it was opened or saved)
+
+    ''' <summary>
+    ''' Private subroutine "CheckSave" (no parameters) - If no file has been opened or saved, but text has been entered into the text box
+    ''' (i.e., into a new file), alert the user that their text will be discarded. For all other situations EXCEPT a blank text box when
+    ''' no file has been opened or saved, check the text box contents against the contents of the file that was opened or saved, and if
+    ''' they differ, alert the user that their changes will be discarded (catch an exception and give another error message if there was
+    ''' a problem accessing the file that was opened or saved).
+    ''' </summary>
+    Private Sub CheckSave()
+        Dim loadedFile As String = ""       '' This will hold the contents of the file that was opened or saved (if applicable)
+
+        '' If the user has entered text (other than white space) into a new file, alert them that the text will be discarded
+        If (Trim(tbMainBox.Text) IsNot "") AndAlso (latestFileName Is "") Then
+            MessageBox.Show("Your text will be discarded!")
+            '' Otherwise, for any situation other than empty text in a new file...
+        ElseIf Not (Trim(tbMainBox.Text) Is "") AndAlso (latestFileName Is "") Then
+            '' Try...
+            Try
+                '' Load the contents of the file that was opened or saved into a StreamReader
+                Dim reader As New StreamReader(latestFileName)
+                '' Use the StreamReader to save those contents to the string variable
+                loadedFile = reader.ReadToEnd
+                '' Close the StreamReader to release the resource
+                reader.Close()
+                '' If the contents of the file do not match the contents of the text box, alert user that the changes will be discarded
+                If Not (loadedFile = tbMainBox.Text) Then
+                    MessageBox.Show("Changes will be discarded!")
+                End If
+                '' If attempting to access the file that was saved or opened causes an exception, give an error message without
+                '' closing the program
+            Catch ex As Exception
+                Throw New ApplicationException(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Exit in the File drop-down menu - Call CheckSave subroutine to give alert if text is being discarded,
+    ''' then close the entire application.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        CheckSave()
+        Application.Exit()
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Open in the File drop-down menu - If user clicks OK in the OpenFileDialog that pops up, call CheckSave
+    ''' subroutine to give alert if text is being discarded, then try to load the contents of the selected file into the text box and append
+    ''' the file name to the text (the program's name) at the top of the window. If an exception is thrown trying to access the file, give an
+    ''' error message without closing the program.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuOpen_Click(sender As Object, e As EventArgs) Handles mnuOpen.Click
+        '' Do the following if the user clicks OK in the Open File Dialog that pops up
+        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
+            '' Give alert if changes will be discarded
+            CheckSave()
+            '' Try to open the file, read its contents, and set the text box contents to the file's contents, but catch an exception if there
+            '' is a problem opening or reading the file, and if so give an error message without closing the program
+            Try
+                Dim reader As New StreamReader(OpenFileDialog.FileName)
+                tbMainBox.Text = reader.ReadToEnd
+                reader.Close()                                      '' Release the resource
+                latestFileName = OpenFileDialog.FileName            '' Save the file name
+                Me.Text = FormNameDefault + " " + latestFileName    '' Add the file name to the program name at the top of the window
+            Catch ex As Exception
+                Throw New ApplicationException(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Save As in the File drop-down menu - In the Save File Dialog that pops up, only allow viewing and
+    ''' saving of text files (this program can only handle text files), and if the user clicks OK in the dialog, save the file name they
+    ''' entered, change the text at the top of the window to reflect that file name, then run the Save subroutine.
+    ''' </summary>
+    Private Sub mnuSaveAs_Click() Handles mnuSaveAs.Click
+        SaveFileDialog.Filter = "TXT Files (*.txt)|*.txt"
+        If SaveFileDialog.ShowDialog() = DialogResult.OK Then
+            latestFileName = SaveFileDialog.FileName
+            Me.Text = FormNameDefault + " " + SaveFileDialog.FileName
+            mnuSave_Click()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Save in the File drop-down menu - If the current text box contents do not have an associated file name
+    ''' (because they have not been opened or saved), run the Save As subroutine, otherwise write all text in the text box to the file name
+    ''' (do not append to the end of that file).
+    ''' </summary>
+    Private Sub mnuSave_Click() Handles mnuSave.Click
+        If latestFileName = "" Then
+            mnuSaveAs_Click()
+        Else
+            My.Computer.FileSystem.WriteAllText(latestFileName, tbMainBox.Text, False)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking New in the File drop-down menu - Call the CheckSave subroutine to give an alert if any text is going
+    ''' to be discarded, then clear the text box, clear the variable storing the name of the latest file that was opened or saved, and
+    ''' reset the text at the top of the window (to the program's name).
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuNew_Click(sender As Object, e As EventArgs) Handles mnuNew.Click
+        CheckSave()
+        tbMainBox.Clear()
+        latestFileName = ""
+        Me.Text = FormNameDefault
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Copy in the Edit drop-down menu - Copy any selected text in the text box to the Windows clipboard.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuCopy_Click(sender As Object, e As EventArgs) Handles mnuCopy.Click
+        tbMainBox.Copy()
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Cut in the Edit drop-down menu - Delete any selected text in the text box and add it to
+    ''' the Windows clipboard.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuCut_Click(sender As Object, e As EventArgs) Handles mnuCut.Click
+        tbMainBox.Cut()
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking Paste in the Edit drop-down menu - Paste the contents of the Windows clipboard at the current
+    ''' cursor location in the text box (this will overwrite any selected text in the text box).
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuPaste_Click(sender As Object, e As EventArgs) Handles mnuPaste.Click
+        tbMainBox.Paste()
+    End Sub
+
+    ''' <summary>
+    ''' Subroutine to handle clicking About in the Help drop-down menu - Pop up a message box displaying the course, assignment number,
+    ''' and author of this program.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mnuAbout_Click(sender As Object, e As EventArgs) Handles mnuAbout.Click
+        MessageBox.Show("NETD-2202" + vbCrLf + "Lab #5" + vbCrLf + "S. Yarrow")
+    End Sub
+End Class
